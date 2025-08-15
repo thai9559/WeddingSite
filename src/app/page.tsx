fdb957f199a10a8c77946baff0dfeff53942d5e3
thai9559 <thai.2006.hyggelig@gmail.com>
2025-08-15 16:54:59 +0700

    fx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 // src/app/page.tsx
 import Header from "./components/Header";
 import { Hero } from "./components/Hero";
-import { Intro } from "./components/Intro";
 import { Approach } from "./components/Approach";
 import { Cards } from "./components/Cards";
 import { SiteFooter } from "./components/SiteFooter";
