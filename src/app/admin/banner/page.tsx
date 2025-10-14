65a015d24a83700938445acedac83dd3d4154c6d
thai9559 <thai.2006.hyggelig@gmail.com>
2025-10-14 15:35:09 +0700

    fx

--- src/app/admin/banner/page.tsx
+++ src/app/admin/banner/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
 
 import { useCallback, useEffect, useState } from "react";
 import { supabaseBrowser } from "@/app/lib/supabase-browser";
